7fd265866d2993f66455f563c041b3ad461f7b5e
Сергей <mitek-metal@bk.ru>
2022-02-09 23:51:28 +0300

    Добавлен класс представляющий сообщение и класс реализующий INotifyPropertyChanged

---
+++ BotModel/Message.cs
@@ -0,0 +1,87 @@
+﻿using BotModel.Base;
+using System.Collections.ObjectModel;
+
+namespace BotModel
+{
+    public class Message : BaseNotificationClass
+    {
+        #region Конструкторы
+
+        public Message() { }
+
+        public Message(
+            string time,
+            string messageText,
+            string firstName,
+            long id)
+        {
+            Time = time;
+            MessageText = messageText;
+            FirstName = firstName;
+            Id = id;
+        }
+
+        #endregion
+
+        #region Поля
+
+        private string _time, _messageText, _firstName;
+        private long _id;
+        private static ObservableCollection<Message> _messages;
+
+        #endregion
+
+        #region Свойства
+
+        public static ObservableCollection<Message> Messages
+        {
+            get
+            {
+                if (_messages == null) _messages = new();
+                return _messages;
+            }
+        }
+
+        public string Time
+        {
+            get => _time;
+            set
+            {
+                _time = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string MessageText
+        {
+            get => _messageText;
+            set
+            {
+                _messageText = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string FirstName
+        {
+            get => _firstName;
+            set
+            {
+                _firstName = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public long Id
+        {
+            get => _id;
+            set
+            {
+                _id = value;
+                OnPropertyChanged();
+            }
+        }
+
+        #endregion
+    }
+}
